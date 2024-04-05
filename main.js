9f9c691bb33d3826fcf9f591108afb7eac35d20c
Cookie <alstrokash@gmail.com>
2024-04-05 20:09:15 +0300

    focus

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const cat = document.querySelector("#maxwell");
 
 cat.addEventListener("click", (e) => {
   for (let i = 0; i < 10; ++i) {
-    window.open("/fuck.html");
+    window.open("/fuck.html").focus();
   }
 });
