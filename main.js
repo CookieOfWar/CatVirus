c23989726239e1bea56e75fb1ae39be0de421997
Cookie <alstrokash@gmail.com>
2024-04-05 21:22:50 +0300

    calculator

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const cat = document.querySelector("#maxwell");
 
 cat.addEventListener("click", (e) => {
-  for (let i = 0; i < 20; ++i) {
-    window.open("/fuck.html");
+  for (let i = 0; i < 5; ++i) {
+    window.open(
+      "file:///C:/Program Files/WindowsApps/Microsoft.WindowsCalculator_11.2401.0.0_x64__8wekyb3d8bbwe"
+    );
   }
 });
